6aaf065f2a124329b3b3067bdbc2a6927824b3ce
Alparslan <alparslanztk@gmail.com>
2024-06-25 19:09:42 +0300

    25.06.24_2

---
+++ tests/Actions.spec.ts
@@ -0,0 +1,14 @@
+import{test,expect} from '@playwright/test';
+
+test.describe("Actions",()=>{
+
+ test("Hover actions",async({page})=>{
+   await page.goto("https://www.amazon.com/");
+   const helloSignInLink=page.locator("#nav-link-accountList");
+   await page.waitForSelector("#nav-link-accountList");
+   await helloSignInLink.hover();
+   await page.waitForTimeout(3000);
+   const accountLink=page.locator("//span[text()='Account']");
+   await accountLink.hover();
+ })
+})
