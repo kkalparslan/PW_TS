7340ba8dc0bc3720ba5501806cde72ac96bc9cb0
Alparslan <alparslanztk@gmail.com>
2024-07-10 00:49:03 +0300

    1 m

--- tests/Actions.spec.ts
+++ tests/Actions.spec.ts
@@ -6,7 +6,7 @@ test.describe("Actions", () => {
     await page.goto("https://www.amazon.com/");
     const helloSignInLink = page.locator("#nav-link-accountList");
     await page.waitForSelector("#nav-link-accountList");
-    await helloSignInLink.hover();
+    await helloSignInLink.hover();n 
     await page.waitForTimeout(3000);
     const accountLink = page.locator("//span[text()='Account']");
     await accountLink.hover();
